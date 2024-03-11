7deb810bf555ac78a0ed0b6a7ef6d68b8c2c6742
ifarnami <farnam.faryar@gmail.com>
2024-03-11 14:08:26 +0330

    chore(tailwind): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          elements: "rgb(43, 57, 69)",
+          bg: "rgb(32, 44, 55)",
+          text: "#ffffff",
+        },
+        light: {
+          elements: "#ffffff",
+          text: "rgb(17, 21, 23)",
+          bg: "#fafafaea",
+          input: "rgb(133, 133, 133)",
+        },
+      },
+    },
   },
   plugins: [],
 };
