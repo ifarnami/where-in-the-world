a7b06a59a657d43af307bf79ee7d9dd22d8ba362
ifarnami <farnam.faryar@gmail.com>
2024-03-11 15:16:30 +0330

    chore(tailwind): add custom box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -16,6 +17,9 @@ export default {
           input: "rgb(133, 133, 133)",
         },
       },
+    },
+    boxShadow: {
+      primary: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
     },
   },
   plugins: [],
