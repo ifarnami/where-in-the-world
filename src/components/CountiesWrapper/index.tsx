9b068b0ece07baef12474ebcf9c6bca7300f5d12
ifarnami <farnam.faryar@gmail.com>
2024-03-12 15:22:10 +0330

    feat(wrapper): add wrapper to render country cards based on fetched data

---
+++ src/components/CountiesWrapper/index.tsx
@@ -0,0 +1,28 @@
+import useCountries from "../../hooks/useCountries";
+import CountryCard from "../CountryCard";
+
+interface ICountriesWrapperProps {}
+
+const CountriesWrapper: React.FC<ICountriesWrapperProps> = (): JSX.Element => {
+  const { countriesList } = useCountries();
+
+  return (
+    <ul className="w-full py-7 grid grid-cols-4 justify-items-center gap-10">
+      {countriesList.map((country, index) => {
+        return (
+          <li key={index}>
+            <CountryCard
+              title={country.name}
+              capital={country.capital}
+              imgAddress={country.flag}
+              population={country.population}
+              region={country.region}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default CountriesWrapper;
