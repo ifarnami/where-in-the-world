ff30ddc2fdfd6a59c75ab04a37f5267c04acd988
ifarnami <farnam.faryar@gmail.com>
2024-03-13 11:05:19 +0330

    chore(tailwind): add daisyUi plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -20,9 +22,9 @@ export default {
     },
     boxShadow: {
       primary:
-        "inset 0 0 0.5px 1px hsla(0, 0%, 100%, 0.075), 0 0 0 1px hsla(0, 0%, 0%, 0.05), 0 0.3px 0.4px hsla(0, 0%, 0%, 0.02), 0 0.9px 1.5px hsla(0, 0%, 0%, 0.045), 0 3.5px 6px hsla(0, 0%, 0%, 0.09)",
-      secondary: "rgba(0, 0, 0, 0.18) 0px 2px 4px",
+        "inset 0 0 0.5px 1px hsla(0, 0%, 100%, 0.075), 0 0 0 1px hsla(0, 0%, 0%, 0.05), 0 0.3px 0.4px hsla(0, 0%, 0%, 0.02), 0 0.9px 1.5px hsla(0, 0%, 0%, 0.045), 0 3.5px 6px hsla(0, 0%, 0%, 0.09) !important",
+      secondary: "rgba(0, 0, 0, 0.18) 0px 2px 4px !important",
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
